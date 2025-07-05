c452ab3441109d29b062d9c6f071d10af912d5eb
Sowjanya134 <sowjanyabathuni405@gmail.com>
2025-07-05 16:10:14 +0530

    notgate

---
+++ notgate/taskn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a=20;
+	int b=10;
+	int c=40;
+	float stmt_1=!(a<=b);
+	float stmt_2=!(c<=b);
+	
+	printf("stmt_1 is:%f",stmt_1);
+	printf("stmt_2 is:%f",stmt_2);
+}
+	
